48b1d32b19aa96d88da2072b9dfe2b3cb09670ba
Marcius Bezerra <marciusbezerra@gmail.com>
2020-01-21 17:50:30 -0300

    Initial Commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Facebook } from '@ionic-native/facebook/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,54 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+
+  // https://www.djamware.com/post/5d949fa24ca99c5ee51238ef/ionic-4-tutorial-facebook-login-example
+
+  isLoggedIn = false;
+  user = { id: '', name: '', email: '', picture: { data: { url: '' } } };
+
+  constructor(private fb: Facebook) {
+    fb.getLoginStatus()
+    .then(
+      res => {
+        console.log(res);
+        this.getUserStatus(res.authResponse.userID);
+        this.isLoggedIn = res.status == 'connected';
+      }
+    )
+    .catch(e => console.error(e))
+    .finally(() => console.log('finish!'));
+  }
+
+  fbLogin() {
+    this.fb.login(['public_profile', 'user_friends', 'email'])
+    .then(res => {
+      if (res.status == 'connected') {
+        this.isLoggedIn = true;
+        this.getUserStatus(res.authResponse.userID);
+      } else {
+        this.isLoggedIn = false;
+      }
+    })
+    .catch(e => console.error(e));
+  }
+
+  getUserStatus(userID: string) {
+    this.fb.api(`/${userID}/?fields=id,email,name,picture`, ['public_profile'])
+    .then(res => {
+      console.log(res);
+      this.user = res;
+    })
+    .catch(e => console.error(e));
+  }
+
+  logout() {
+    this.fb.logout().then(res => {
+      console.log(res);
+      this.isLoggedIn = false;
+    })
+    .catch(e => console.error(e))
+    .finally(() => console.log('finish!'));
+  }
 
 }
